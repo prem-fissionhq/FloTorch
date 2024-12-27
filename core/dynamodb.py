c8f7e9c54cb1d6b9373065940bacdf24d1207f34
DESKTOP-C010PSV\FL_LPT-470 <adil.raza@fissionlabs.com>
2024-12-27 17:00:44 +0530

    fix last index in dynamo db

--- core/dynamodb.py
+++ core/dynamodb.py
@@ -69,19 +69,25 @@ class DynamoDBOperations:
         Returns:
             Dict: Scan results.
         """
-        result = []
+        result = {
+            "Items": []
+        }
+        last_evaluated_key = None
         try:
             params = {}
-            if filter_expression:
-                params["FilterExpression"] = filter_expression
-            if expression_values:
-                params["ExpressionAttributeValues"] = self._handle_decimal_type(expression_values)
-            if expression_attribute_names:
-                params["ExpressionAttributeNames"] = expression_attribute_names
 
             while True:
+                if filter_expression:
+                    params["FilterExpression"] = filter_expression
+                if expression_values:
+                    params["ExpressionAttributeValues"] = self._handle_decimal_type(expression_values)
+                if expression_attribute_names:
+                    params["ExpressionAttributeNames"] = expression_attribute_names
+                if last_evaluated_key:
+                    params['ExclusiveStartKey'] = last_evaluated_key
+
                 response = self.table.scan(**params)
-                result.extend(response.get('Items', []))
+                result["Items"].extend(response.get('Items', []))
                 last_evaluated_key = response.get('LastEvaluatedKey')
 
                 if not last_evaluated_key:
